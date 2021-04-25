6757cf336863242ecc025862d5edada6e4374317
jonathangonzalezlpz <jonathan.gonzalezlpz@gmail.com>
2021-04-25 12:49:51 +0200

    Jony: añado una clase Exception de especificacion de errores

--- src/Model/OrdenTrabajo.java
+++ src/Model/OrdenTrabajo.java
@@ -1,5 +1,136 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class OrdenTrabajo {
+	
+	private Integer identificador;
+	private String descripcion;
+	private ArrayList<String> material;
+	private ArrayList<Presupuesto> presupuesto;
+	private Double coste;
+	private String responsable;
+	private Integer personal;
+	private Date fechaInicio;
+	private Integer duracion;
+	private String estado;
+	private Proceso proceso;
+	
+	//Constructor
+	//Vacio para inicializar libremente con setters 
+	public OrdenTrabajo() {
+		
+	}
+	
+	public OrdenTrabajo(Integer identificador, String descripcion, 
+			ArrayList<String> material, ArrayList<Presupuesto> presupuesto,
+			Double coste, String responsable, Integer personal, Date fechaInicio, Integer duracion, 
+			String estado, Proceso proceso) {
+		this.identificador = identificador;
+		this.descripcion = descripcion;
+		this.material = material;
+		this.presupuesto = presupuesto;
+		this.coste = coste;
+		this.responsable = responsable;
+		this.personal = personal;
+		this.fechaInicio = fechaInicio;
+		this.duracion = duracion;
+		this.estado = estado;
+		this.proceso = proceso;
+	}
+	
+	
+	//GETTERS Y SETTERS
+	public Integer getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(Integer identificador) {
+		this.identificador = identificador;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public ArrayList<String> getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(ArrayList<String> material) {
+		this.material = material;
+	}
+
+	public ArrayList<Presupuesto> getPresupuesto() {
+		return presupuesto;
+	}
+
+	public void setPresupuesto(ArrayList<Presupuesto> presupuesto) {
+		this.presupuesto = presupuesto;
+	}
+
+	public Double getCoste() {
+		return coste;
+	}
+
+	public void setCoste(Double coste) {
+		this.coste = coste;
+	}
+
+	public String getResponsable() {
+		return responsable;
+	}
+
+	public void setResponsable(String responsable) {
+		this.responsable = responsable;
+	}
+
+	public Integer getPersonal() {
+		return personal;
+	}
+
+	public void setPersonal(Integer personal) {
+		this.personal = personal;
+	}
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Integer getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(Integer duracion) {
+		this.duracion = duracion;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Proceso getProceso() {
+		return proceso;
+	}
+
+	public void setProceso(Proceso proceso) {
+		this.proceso = proceso;
+	}
+	
+	
+	
 
 }
--- src/Model/Presupuesto.java
+++ src/Model/Presupuesto.java
@@ -1,5 +1,94 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Presupuesto {
 
+	private Integer identificador;
+	private String empresa;
+	private Double presupuesto;
+	private Date fechaInicio;
+	private Integer duracion;
+	private ArrayList<String> material;
+	private Integer personal;
+	
+	//Constructor
+	//vacío para construcción con setters
+	public Presupuesto() {
+		
+	}
+	
+	//Con todos los parametros
+	public Presupuesto(Integer identificador, String empresa, Double presupuesto, Date fechaInicio, Integer duracion,
+			ArrayList<String> material, Integer personal) {
+		this.identificador = identificador;
+		this.empresa = empresa;
+		this.presupuesto = presupuesto;
+		this.fechaInicio = fechaInicio;
+		this.duracion = duracion;
+		this.material = material;
+		this.personal = personal;
+	}
+
+	//Getters y Setters
+	public Integer getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(Integer identificador) {
+		this.identificador = identificador;
+	}
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+	public Double getPresupuesto() {
+		return presupuesto;
+	}
+
+	public void setPresupuesto(Double presupuesto) {
+		this.presupuesto = presupuesto;
+	}
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Integer getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(Integer duracion) {
+		this.duracion = duracion;
+	}
+
+	public ArrayList<String> getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(ArrayList<String> material) {
+		this.material = material;
+	}
+
+	public Integer getPersonal() {
+		return personal;
+	}
+
+	public void setPersonal(Integer personal) {
+		this.personal = personal;
+	}
+	
+		
+	
+	
 }
