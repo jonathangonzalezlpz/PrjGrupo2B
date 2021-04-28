5b894d18adc212b0b14b755d70d4e6856d0f36ca
iagofts <64792716+iagofts@users.noreply.github.com>
2021-04-28 15:54:42 +0200

    Implementacion de SubsistemaAnalisisYEstadisticas

--- src/SubsistemaAnalisisYEstadisticas/SubsistemaAnalisisEstadisticas.java
+++ src/SubsistemaAnalisisYEstadisticas/SubsistemaAnalisisEstadisticas.java
@@ -143,23 +143,245 @@ public class SubsistemaAnalisisEstadisticas implements InterfaceSubsistemaAnalis
 		double costeFinal=0;
 		for(Estadistica e: HMDistribucion.values()) {
 			nFinal+=e.getNumeroTotal();
-			costeFinal+=e.getCoste();
+			costeFinal+=e.getCoste()*e.getNumeroTotal();
 		}
-		estadisticaFinal.setCoste(costeFinal);
+		estadisticaFinal.setCoste(costeFinal/nFinal);
 		estadisticaFinal.setNumeroTotal(nFinal);
 		return estadisticaFinal;
 	}
 
 	@Override
-	public Estadistica obtenerEstadisticasProcesos(String rango, Proceso filtro, String distribucion) {
-		// TODO Auto-generated method stub
-		return null;
+	public Estadistica obtenerEstadisticasProcesos(String rango, Proceso filtro, String distribucion) 
+		throws CustomException {
+
+		ArrayList<Proceso> procesos;
+		ArrayList<Proceso> procesosFiltrados = new ArrayList<>();
+
+		if (distribucion != "dia" && distribucion != "semana") {
+			throw new CustomException("Distribucion seleccionada incorrecta", 1);
+		}
+
+		String[] partes = rango.split("-");
+		if (partes.length != 2) {
+			throw new CustomException("Fecha invalida", 1);
+		}
+
+		String[] splittedFecha1 = partes[0].split("/");
+		String[] splittedFecha2 = partes[1].split("/");
+		if (splittedFecha1.length != 3 || splittedFecha2.length != 3) {
+			throw new CustomException("Fecha invalida", 1);
+		}
+
+		int ano1 = Integer.parseInt(splittedFecha1[2]);
+		int mes1 = Integer.parseInt(splittedFecha1[1]);
+		int dia1 = Integer.parseInt(splittedFecha1[0]);
+
+		int ano2 = Integer.parseInt(splittedFecha2[2]);
+		int mes2 = Integer.parseInt(splittedFecha2[1]);
+		int dia2 = Integer.parseInt(splittedFecha2[0]);
+
+		if (ano1 < 1 || ano2 < 1 || mes1 < 1 || mes2 < 1 || mes1 > 12 || mes2 > 12 || dia1 < 1 || dia2 < 1 || dia1 > 31
+				|| dia2 > 31) {
+			throw new CustomException("Fecha invalida", 1);
+		}
+
+		Date fecha1 = new Date(ano1 - 1900, mes1 - 1, dia1);
+		Date fecha2 = new Date(ano2 - 1900, mes2 - 1, dia2);
+
+		if (fecha1.after(fecha2)) {
+			throw new CustomException("Primera fecha menor que la segunda fecha", 1);
+		}
+
+		Date fechaMinima = new Date(200, 0, 1);
+		Date fechaMaxima = new Date(0, 0, 1);
+		procesos = gp.buscar(filtro);
+
+		if (procesos == null || procesos.size() == 0) {
+			throw new CustomException("No existen procesos en este rango de fechas", 1);
+		}
+		for (Proceso i : procesos) {
+			Date fecha = i.getFechaInicio();
+			if (fecha.after(fecha1) && fecha.before(fecha2)) {
+				procesosFiltrados.add(i);
+
+				// Comprueba en cada iteraccion si la fecha introducida es menor que la supuesta
+				// minima
+				if (i.getFechaInicio().compareTo(fechaMinima) < 0) {
+					fechaMinima = i.getFechaInicio();
+				}
+
+				// Comprueba en cada iteraccion si la fecha introducida es mayor que la supuesta
+				// maxima
+				if (i.getFechaInicio().compareTo(fechaMaxima) > 0) {
+					fechaMaxima = i.getFechaInicio();
+				}
+			}
+
+		}
+		Estadistica estadisticaFinal = new Estadistica();
+		HashMap<String, Estadistica> HMDistribucion = new HashMap<>();
+		Date fechaActual = fechaMinima;
+		int nProcesos;
+		double coste;
+		if (distribucion == "dia") {
+			while (fechaActual.compareTo(fechaMaxima) <= 0) {
+				nProcesos = 0;
+				coste = 0.0;
+				for (Proceso i : procesosFiltrados) {
+					if (i.getFechaInicio().compareTo(fechaActual) == 0) {
+						nProcesos++;
+						coste += i.getCoste();
+						//Aqui no puedo eliminar los q si pasan el filtro para que no se repitan despues?
+					}
+				}
+				HMDistribucion.put(fechaActual.getDay() + "/" + fechaActual.getMonth() + "/" + fechaActual.getYear(),
+						new Estadistica(nProcesos, coste / nProcesos));
+
+				fechaActual.setDate(fechaActual.getDate() + 1);
+			}
+		} else {
+			while (fechaActual.compareTo(fechaMaxima) <= 0) {
+				nProcesos = 0;
+				coste = 0.0;
+				for (Proceso i : procesosFiltrados) {
+					if (i.getFechaInicio().getTime()>= fechaActual.getTime() && i.getFechaInicio().getTime()< fechaActual.getTime()+604800000) {
+						nProcesos++;
+						coste += i.getCoste();
+					}
+				}
+				HMDistribucion.put(fechaActual.getDay() + "/" + fechaActual.getMonth() + "/" + fechaActual.getYear(),
+						new Estadistica(nProcesos, coste / nProcesos));
+
+				fechaActual.setDate(fechaActual.getDate() + 7);
+			}
+		}
+		estadisticaFinal.setDistribucion(HMDistribucion);
+		int nFinal=0;
+		double costeFinal=0;
+		for(Estadistica e: HMDistribucion.values()) {
+			nFinal+=e.getNumeroTotal();
+			costeFinal+=e.getCoste()*e.getNumeroTotal();
+		}
+		estadisticaFinal.setCoste(costeFinal/nFinal);
+		estadisticaFinal.setNumeroTotal(nFinal);
+		return estadisticaFinal;
 	}
 
 	@Override
-	public Estadistica obtenerEstadisticasOrdenesTrabajo(String rango, OrdenTrabajo filtro, String distribucion) {
-		// TODO Auto-generated method stub
-		return null;
+	public Estadistica obtenerEstadisticasOrdenesTrabajo(String rango, OrdenTrabajo filtro, String distribucion)
+		throws CustomException {
+
+		ArrayList<OrdenTrabajo> ordenesTrabajo;
+		ArrayList<OrdenTrabajo> ordenesTrabajoFiltradas = new ArrayList<>();
+
+		if (distribucion != "dia" && distribucion != "semana") {
+			throw new CustomException("Distribucion seleccionada incorrecta", 1);
+		}
+
+		String[] partes = rango.split("-");
+		if (partes.length != 2) {
+			throw new CustomException("Fecha invalida", 1);
+		}
+
+		String[] splittedFecha1 = partes[0].split("/");
+		String[] splittedFecha2 = partes[1].split("/");
+		if (splittedFecha1.length != 3 || splittedFecha2.length != 3) {
+			throw new CustomException("Fecha invalida", 1);
+		}
+
+		int ano1 = Integer.parseInt(splittedFecha1[2]);
+		int mes1 = Integer.parseInt(splittedFecha1[1]);
+		int dia1 = Integer.parseInt(splittedFecha1[0]);
+
+		int ano2 = Integer.parseInt(splittedFecha2[2]);
+		int mes2 = Integer.parseInt(splittedFecha2[1]);
+		int dia2 = Integer.parseInt(splittedFecha2[0]);
+
+		if (ano1 < 1 || ano2 < 1 || mes1 < 1 || mes2 < 1 || mes1 > 12 || mes2 > 12 || dia1 < 1 || dia2 < 1 || dia1 > 31
+				|| dia2 > 31) {
+			throw new CustomException("Fecha invalida", 1);
+		}
+
+		Date fecha1 = new Date(ano1 - 1900, mes1 - 1, dia1);
+		Date fecha2 = new Date(ano2 - 1900, mes2 - 1, dia2);
+
+		if (fecha1.after(fecha2)) {
+			throw new CustomException("Primera fecha menor que la segunda fecha", 1);
+		}
+
+		Date fechaMinima = new Date(200, 0, 1);
+		Date fechaMaxima = new Date(0, 0, 1);
+		ordenesTrabajo = got.buscar(filtro);
+
+		if (ordenesTrabajo == null || ordenesTrabajo.size() == 0) {
+			throw new CustomException("No existen ordenes de trabajo en este rango de fechas", 1);
+		}
+		for (OrdenTrabajo i : ordenesTrabajo) {
+			Date fecha = i.getFechaInicio();
+			if (fecha.after(fecha1) && fecha.before(fecha2)) {
+				ordenesTrabajoFiltradas.add(i);
+
+				// Comprueba en cada iteraccion si la fecha introducida es menor que la supuesta
+				// minima
+				if (i.getFechaInicio().compareTo(fechaMinima) < 0) {
+					fechaMinima = i.getFechaInicio();
+				}
+
+				// Comprueba en cada iteraccion si la fecha introducida es mayor que la supuesta
+				// maxima
+				if (i.getFechaInicio().compareTo(fechaMaxima) > 0) {
+					fechaMaxima = i.getFechaInicio();
+				}
+			}
+
+		}
+		Estadistica estadisticaFinal = new Estadistica();
+		HashMap<String, Estadistica> HMDistribucion = new HashMap<>();
+		Date fechaActual = fechaMinima;
+		int nOrdenTrabajos;
+		double coste;
+		if (distribucion == "dia") {
+			while (fechaActual.compareTo(fechaMaxima) <= 0) {
+				nOrdenTrabajos = 0;
+				coste = 0.0;
+				for (OrdenTrabajo i : ordenesTrabajoFiltradas) {
+					if (i.getFechaInicio().compareTo(fechaActual) == 0) {
+						nOrdenTrabajos++;
+						coste += i.getCoste();
+						//Aqui no puedo eliminar los q si pasan el filtro para que no se repitan despues?
+					}
+				}
+				HMDistribucion.put(fechaActual.getDay() + "/" + fechaActual.getMonth() + "/" + fechaActual.getYear(),
+						new Estadistica(nOrdenTrabajos, coste / nOrdenTrabajos));
+
+				fechaActual.setDate(fechaActual.getDate() + 1);
+			}
+		} else {
+			while (fechaActual.compareTo(fechaMaxima) <= 0) {
+				nOrdenTrabajos = 0;
+				coste = 0.0;
+				for (OrdenTrabajo i : ordenesTrabajoFiltradas) {
+					if (i.getFechaInicio().getTime()>= fechaActual.getTime() && i.getFechaInicio().getTime()< fechaActual.getTime()+604800000) {
+						nOrdenTrabajos++;
+						coste += i.getCoste();
+					}
+				}
+				HMDistribucion.put(fechaActual.getDay() + "/" + fechaActual.getMonth() + "/" + fechaActual.getYear(),
+						new Estadistica(nOrdenTrabajos, coste / nOrdenTrabajos));
+
+				fechaActual.setDate(fechaActual.getDate() + 7);
+			}
+		}
+		estadisticaFinal.setDistribucion(HMDistribucion);
+		int nFinal=0;
+		double costeFinal=0;
+		for(Estadistica e: HMDistribucion.values()) {
+			nFinal+=e.getNumeroTotal();
+			costeFinal+=e.getCoste()*e.getNumeroTotal();
+		}
+		estadisticaFinal.setCoste(costeFinal/nFinal);
+		estadisticaFinal.setNumeroTotal(nFinal);
+		return estadisticaFinal;
 	}
 
 }
